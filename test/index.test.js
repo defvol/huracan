fed54424a1e8cb064a84ecdc48a752fcc5001442
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-07-06 22:20:10 -0400

    Remove async callback from summary func

--- test/index.test.js
+++ test/index.test.js
@@ -10,11 +10,8 @@ test('advisory', function (t) {
 })
 
 test('summary', function (t) {
-  t.plan(2);
-
   var fixture = fs.readFileSync(__dirname + '/fixtures/gis-ep.xml');
-  huracan.summary(fixture, function (err, res) {
-    t.error(err, 'no errors');
-    t.true(res.match('nhc.noaa.gov'), 'finds a link to the summary');
-  });
+  var link = huracan.summary(fixture);
+  t.true(link.match('nhc.noaa.gov'), 'finds a link to the summary');
+  t.end();
 });
