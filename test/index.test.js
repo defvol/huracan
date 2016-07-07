a8f5cf006ebdea3d860f803978c5e46860fc6927
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-07-06 22:50:19 -0400

    Fetch the real thing from NOAA

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,36 @@
-var fs = require('fs')
+var fs = require('fs');
+var nock = require('nock');
 var test = require('tape');
 var huracan = require('../lib/index');
 
+var fixtures = {
+  advisory: fs.readFileSync(__dirname + '/fixtures/062034.shtml').toString(),
+  feed: fs.readFileSync(__dirname + '/fixtures/gis-ep.xml')
+};
+
+var noaa = nock('http://www.nhc.noaa.gov')
+  .get('/gis-ep.xml')
+  .reply(200, fixtures.feed)
+  .get(/text\/refresh\/MIATCPEP3/)
+  .reply(200, fixtures.advisory);
+
 test('advisory', function (t) {
-  var fixture = fs.readFileSync(__dirname + '/fixtures/062034.shtml');
-  var info = huracan.advisory(fixture.toString());
+  var info = huracan.advisory(fixtures.advisory);
   t.deepEqual(info.location, [15.2, -124.7], 'scrapes location');
   t.end();
-})
+});
+
+test('fetch', function (t) {
+  t.plan(2);
+  var want = { location: [15.2, -124.7] };
+  huracan.fetch(function (err, info) {
+    t.error(err, 'with no errors');
+    t.deepEqual(info, want, 'finds the hurricane');
+  });
+});
 
 test('summary', function (t) {
-  var fixture = fs.readFileSync(__dirname + '/fixtures/gis-ep.xml');
-  var link = huracan.summary(fixture);
+  var link = huracan.summary(fixtures.feed);
   t.true(link.match('nhc.noaa.gov'), 'finds a link to the summary');
   t.end();
 });
